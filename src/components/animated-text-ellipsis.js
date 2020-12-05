75decc4d796296f478f1bb9a08a89980e9c68dbe
Tony Nguyen <tony.nguyen1799@gmail.com>
2020-12-05 19:02:11 +0700

    Add Splash screen

---
+++ src/components/animated-text-ellipsis.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AnimatedTextEllipsis = ({ text = '', count = 3, delay = 1000, style = {}, textStyle = {} }) => {
+    const ellipsisCountRef = useRef(0);
+    const [showingEllipsisCount, setShowingEllipsisCount] = useState(ellipsisCountRef.current);
+
+    useEffect(() => {
+        const syncEllipsisInterval = setInterval(() => {
+            ellipsisCountRef.current = ellipsisCountRef.current < count ? (++ellipsisCountRef.current) : 0
+            setShowingEllipsisCount(ellipsisCountRef.current);
+        }, delay);
+
+        return () => clearInterval(syncEllipsisInterval);
+    }, []);
+
+    return (
+        <View style={{ ...style, ...styles.container }}>
+            <Text style={{ ...textStyle }}>{`${text}`}</Text>
+            {Array(count).fill().map((_, i) =>
+                <Text key={i} style={(i + 1) > showingEllipsisCount ? { ...textStyle, opacity: 0 } : { ...textStyle }}>.</Text>)}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row'
+    },
+});
+
+export default AnimatedTextEllipsis;
---
+++ src/utils/variables.js
@@ -0,0 +1,16 @@
+const defaultFontSize = 16;
+const mediumFontSize = 22;
+const largeFontSize = 28;
+
+const defaultPadding = 20;
+const defaultMargin = 20;
+
+const Variables = {
+    defaultFontSize,
+    mediumFontSize,
+    largeFontSize,
+    defaultPadding,
+    defaultMargin
+}
+
+export default Variables;
