fc9d2dbfe53ea950ee32581522ad72c57b74d008
Tony Nguyen <tony.nguyen1799@gmail.com>
2020-12-29 22:46:29 +0700

    Add font for logo icon

--- src/screens/splash.js
+++ src/screens/splash.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-import Logo from '../../assets/logo.png';
+import { StyleSheet, Text, View } from 'react-native';
 import AnimatedTextEllipsis from '../components/animated-text-ellipsis';
 import Colors from '../utils/colors';
 import Variables from '../utils/variables';
+import Logo from '../components/logo';
 
 const SplashScreen = () => {
     return (
         <View style={styles.container}>
-            <Image source={Logo} />
+            <Logo />
             <AnimatedTextEllipsis style={styles.message} textStyle={styles.message__text} text="Đồng bộ dữ liệu" />
         </View>
     )
@@ -20,6 +20,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: Colors.primaryBackgroundColor
+    },
+    bee__logo: {
+        fontFamily: 'AmaticSC-Bold',
+        fontSize: 256,
+        color: Colors.primaryColor,
+        borderColor: 'red',
+        borderWidth: 1,
+        padding: 0
     },
     message: {
         marginTop: Variables.defaultMargin
